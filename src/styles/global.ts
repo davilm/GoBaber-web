bb8e7ecb50760c3d937c99777c430bceef7f4d30
Davi Leal <lealm.davi@gmail.com>
2020-08-12 12:10:48 -0300

    login page

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -14,15 +14,15 @@ export default createGlobalStyle`
     --webkit-font-smoothing: antialiased;
   }
 
-  border-style, input, button {
+  body, input, button {
     font-family: 'Roboto Slab', serif;
     font-size: 16px;
   }
 
   h1, h2, h3, h4, h5, h6, strong {
-    font-weigt: 500;
+    font-weight: 500;
   }
-  
+
   button {
     cursor: pointer;
   }
